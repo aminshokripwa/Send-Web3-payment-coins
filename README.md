b80fa148833efa1eaf5805511de86d7ec18db4d0
aminshokripwa <102459205+aminshokripwa@users.noreply.github.com>
2022-09-16 03:50:39 +0430

    v1

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Send-Web3-payment-coins
-Send Web3 payment to other wallet
+
+Ensure you create directory in your directory.
+
+`git clone https://github.com/aminshokripwa/Send-Web3-payment-coins.git`
+
+## Download the packages used to create this rest API
+Run the following commands to install all the necessary packages.
+
+```
+npm install --legacy-peer-deps
+```
+
+## Setting configuration file
+
+You can use Metamask plugin for your browser and use Ganache for send coin in your local
+
+### Important!
+
+To run this project connecting to a local chain download Ganache utilize.
+[Download Ganache](https://www.trufflesuite.com/ganache)
+
+Start the ganache blockchain and copy the private key of the desired account.
+
+![Image of Project](/public/images/screen_recording.gif)
+
+Add the local blockchain network in MetaMask by entering the RPC URL and Chain ID
+
+![Image of Project](/public/images/screen_recording.gif)
+
+Add new user with recived adress
+
+![Image of Project](/public/images/screen_recording.gif)
+
+In Metamask choose recent user and network
+
+## Running the project
+
+`npm start`
+
---
+++ src/App.js
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import { ethers } from "ethers";
+import ErrorMessage from "./ErrorMessage";
+import TxList from "./TxList";
+
+const startPayment = async ({ setError, setTxs, ether, addr }) => {
+  try {
+    if (!window.ethereum)
+      throw new Error("No crypto wallet found. Please install it.");
+
+    await window.ethereum.send("eth_requestAccounts");
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    ethers.utils.getAddress(addr);
+    const tx = await signer.sendTransaction({
+      to: addr,
+      value: ethers.utils.parseEther(ether)
+    });
+    console.log({ ether, addr });
+    console.log("tx", tx);
+    setTxs([tx]);
+  } catch (err) {
+    setError(err.message);
+  }
+};
+
+export default function App() {
+  const [error, setError] = useState();
+  const [txs, setTxs] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    setError();
+    await startPayment({
+      setError,
+      setTxs,
+      ether: data.get("ether"),
+      addr: data.get("addr")
+    });
+  };
+
+  return (
+    <form className="m-4" onSubmit={handleSubmit}>
+      <div className="credit-card w-full lg:w-1/2 sm:w-auto shadow-lg mx-auto rounded-xl bg-white">
+        <main className="mt-4 p-4">
+          <h1 className="text-xl font-semibold text-gray-700 text-center">
+            Send ETH payment
+          </h1>
+          <div className="">
+            <div className="my-3">
+              <input
+                type="text"
+                name="addr"
+                className="input input-bordered block w-full focus:ring focus:outline-none"
+                placeholder="Recipient Address"
+              />
+            </div>
+            <div className="my-3">
+              <input
+                name="ether"
+                type="text"
+                className="input input-bordered block w-full focus:ring focus:outline-none"
+                placeholder="Amount in ETH"
+              />
+            </div>
+          </div>
+        </main>
+        <footer className="p-4">
+          <button
+            type="submit"
+            className="btn btn-primary submit-button focus:ring focus:outline-none w-full"
+          >
+            Pay now
+          </button>
+          <ErrorMessage message={error} />
+          <TxList txs={txs} />
+        </footer>
+      </div>
+    </form>
+  );
+}
